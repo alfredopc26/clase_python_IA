2c4db1600b08dd3f551a9bd7d25be81a510f9a5c
Alfredo Perez <carlos.perez@jobgether.com>
2021-08-30 20:34:48 -0500

    Suma de dos numeros e imprimir resultado

--- clase_uno.py
+++ clase_uno.py
@@ -5,4 +5,9 @@ Created on Mon Aug 30 18:51:46 2021
 @author: Carlos Perez
 """
 
-print('ok')
+# Sume dos numeros e imprima su resultado 
+
+numA = input('Para la suma de A + B. Digite el numero A: \n')
+numB = input('Para la suma de A + B. Digite el numero B: \n')
+
+print("El resultado es: " + str(int(numA) + int(numB)))
