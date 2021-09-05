f0473c6385248119ebf66ac411a91de4a36fd99d
Alfredo Perez <carlos.perez@jobgether.com>
2021-09-04 22:20:52 -0500

    Taller punto uno ecuacion

---
+++ punto_uno.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep  4 22:19:21 2021
+
+@author: Carlos Perez
+"""
+
+# Calcule el valor de Y indicando paso a paso como llego al resultado
+# Punto 1
+# Se crea la variable y con la ecuacion
+y = ((5 + 2 - 5) ** 2 * 5 + 8 / 2 - 30) / 2 * 5 - 3
+# Esta se valida con un print
+print(y)
