d6352e8d9f1097fb3e8059787556d0fc0ff56cef
Alfredo Perez <carlos.perez@jobgether.com>
2021-09-04 22:21:33 -0500

    Taller punto tres ecuacion

--- punto_uno.py
+++ punto_uno.py
@@ -23,3 +23,18 @@ Y = (((z + 2 - n) ** 2 * m + 8 / 2 - 30) / 2 * 5 - 3) ** 5 + 15 * 3 - 9 / 3
 # Se imprime el resultado
 print(Y)
 # Este imprime el resultado 248874.0
+#
+# Punto 3
+# Se crean las variables Z, N, M
+z = 5
+n = ((8 + 2 - 4) ** 2 * 5 + 8 + 7 / 2 - 30 * 5) / 2 * 5 - 3
+m = z ** 2 * 3 + n
+# Se crea la ecuacion con la variable Y junto con las demas
+# Como la ecuacion es muy larga y supera la reglamentacion PEP8
+# Debemos construir una variable auxiliar que nos facilite el trabajo
+# a esta la llamaremos Y1
+Y1 = ((z + 2 - n) ** 2 * m + 8 / 2 - 30)
+Y = ((Y1 / 2 * 5 - 3) ** 5 + 15 * 3 - 9 / 3) ** 2 - 5 / 4
+# Imprimimos el valor de Y
+print(Y)
+# Este nos da 7.373988851809673e+65
