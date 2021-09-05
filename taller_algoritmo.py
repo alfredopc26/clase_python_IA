6a9c90c4c3ec5b35030366e13d7c6dae0e016ae6
Alfredo Perez <carlos.perez@jobgether.com>
2021-09-04 20:31:30 -0500

    Taller punto dos calcular num pulsaciones

--- taller_algoritmo.py
+++ taller_algoritmo.py
@@ -20,3 +20,17 @@ presion = float(input("Digite la presion : \n"))
 vol = float(input("Digite el volumen : \n"))
 temp = float(input("Digite la temperatura : \n"))
 print(f"La masa calculada es {calcularMasa(presion, vol, temp)}")
+
+
+# Ejercicio 2
+
+def calcularNumPulsaciones(edad):
+    
+    pulsaciones = (200 - edad) / 10
+    
+    return pulsaciones
+
+print("Se va a calcular el numero de pulsaciones.")
+edad = int(input("Digite su edad: \n"))
+print("El numero de pulsaciones que debe tener por 10 seg es :")
+print(calcularNumPulsaciones(edad))
