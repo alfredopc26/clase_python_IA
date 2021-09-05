1da2ba8b8c8e451ee4f1bc44d38d2c65659ca17c
Alfredo Perez <carlos.perez@jobgether.com>
2021-09-04 20:44:53 -0500

    Taller punto tres calcular porcentaje inversion

--- taller_algoritmo.py
+++ taller_algoritmo.py
@@ -34,3 +34,28 @@ print("Se va a calcular el numero de pulsaciones.")
 edad = int(input("Digite su edad: \n"))
 print("El numero de pulsaciones que debe tener por 10 seg es :")
 print(calcularNumPulsaciones(edad))
+
+
+# Ejercicio 3
+
+def calcularPorcentaje(dinTotal, dinInver):
+    
+    porcentaje = (dinInver * 100) / dinTotal
+    
+    return porcentaje
+
+print("Se va a calcular el porcentaje de inversion de cada persona.")
+print("Persona 1")
+persona1 = float(input("Digite su dinero a invertir: \n"))
+print("Persona 2")
+persona2 = float(input("Digite su dinero a invertir: \n"))
+print("Persona 3")
+persona3 = float(input("Digite su dinero a invertir: \n"))
+dinTotal = persona1 + persona2 + persona3
+print("El porcentaje de la Persona 1 es: ")
+print(f"% {calcularPorcentaje(dinTotal, persona1)}")
+print("El porcentaje de la Persona 2 es: ")
+print(f"% {calcularPorcentaje(dinTotal, persona2)}")
+print("El porcentaje de la Persona 3 es: ")
+print(f"% {calcularPorcentaje(dinTotal, persona3)}")
+
