491e29b2abb84b76f18fa90e35f2199bd2b72eff
Alfredo Perez <carlos.perez@jobgether.com>
2021-09-04 22:21:17 -0500

    Taller punto dos ecuacion

--- punto_uno.py
+++ punto_uno.py
@@ -11,3 +11,15 @@ Created on Sat Sep  4 22:19:21 2021
 y = ((5 + 2 - 5) ** 2 * 5 + 8 / 2 - 30) / 2 * 5 - 3
 # Esta se valida con un print
 print(y)
+# Este imprime como resultado -18
+#
+# Punto 2
+# Se crean las variables z, n, m
+z = 5
+n = 3
+m = z - n
+# Se crea la ecuacion con la variable Y y las variables configuradas
+Y = (((z + 2 - n) ** 2 * m + 8 / 2 - 30) / 2 * 5 - 3) ** 5 + 15 * 3 - 9 / 3
+# Se imprime el resultado
+print(Y)
+# Este imprime el resultado 248874.0
